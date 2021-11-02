99409b77b881b7b2edabf1bc87cdfa147ab17f75
hongnhat195 <76255792+hongnhat195@users.noreply.github.com>
2021-11-02 22:47:16 +0700

    Update index.jsx

--- Font/src/container/Client/index.jsx
+++ Font/src/container/Client/index.jsx
@@ -3,14 +3,7 @@ import Footer from "../../components/Footer-Client/footer";
 import Header from "../../components/Header-Client/header";
 import { Route } from "react-router-dom";
 function LayoutHome(props) {
-<<<<<<< HEAD
-<<<<<<< HEAD
-  const path = props.children.props.path;
-=======
->>>>>>> nhat
-=======
-  // const path = props.children.props.path;
->>>>>>> a022d41766c16528cb2fca8dc82ef9889a35e14a
+
   return (
     <>
       <Header />
