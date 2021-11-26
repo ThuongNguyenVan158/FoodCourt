0df08282c0f21ddcda9337bed0b90bb55b4cd384
Elvis Nhat <hongnhat195>
2021-11-26 23:22:51 +0700

    push

--- Backend/routers/order.router.js
+++ Backend/routers/order.router.js
@@ -1,23 +1,35 @@
 import express from "express";
-import { ordering, viewOrderByCustomer,viewListOrder,viewListOrderToday,viewListOrderByDate,viewListOrderByMonth,viewListOrderByYear } from "../controllers/order.controller";
+import {
+  ordering,
+  viewOrderByCustomer,
+  viewListOrder,
+  viewListOrderToday,
+  viewListOrderByDate,
+  viewListOrderByMonth,
+  viewListOrderByYear,
+} from "../controllers/order.controller";
 import { authenticate } from "../middlewares/Auth/authenticate";
 const orderRouter = express.Router();
 orderRouter.post("/order", authenticate, ordering);
 orderRouter.get("/list/:id", authenticate, viewOrderByCustomer);
 orderRouter.get(
-    "/listallorder", 
-    // authenticate, 
-    viewListOrder);
+  "/listallorder",
+  // authenticate,
+  viewListOrder
+);
 orderRouter.get(
-        "/listallordertoday", 
-        // authenticate, 
-        viewListOrderToday);
- orderRouter.get(
-    "/listallorderbydate/:id", 
-    // authenticate, 
-    viewListOrderByDate);
+  "/listallordertoday",
+  // authenticate,
+  viewListOrderToday
+);
 orderRouter.get(
-"/listallorderbymonth", 
-// authenticate, 
-viewListOrderByMonth);
+  "/listallorderbydate/:id",
+  // authenticate,
+  viewListOrderByDate
+);
+orderRouter.get(
+  "/listallorderbymonth",
+  // authenticate,
+  viewListOrderByMonth
+);
 export { orderRouter };
